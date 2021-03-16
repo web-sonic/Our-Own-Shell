b528d1624a155786e81a1780ffe474b331336288
hille2 <hille.2@ya.ru>
2021-03-16 15:17:35 +0300

    kkk

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: sgath <sgath@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 14:02:48 by sgath             #+#    #+#             */
-/*   Updated: 2021/03/16 14:14:16 by sgath            ###   ########.fr       */
+/*   Updated: 2021/03/16 15:14:42 by sgath            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <signal.h>
-#include <dirent.h>
+# include <dirent.h>
 # include <sys/errno.h>
 # include <curses.h>
 # include <term.h>
