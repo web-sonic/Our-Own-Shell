221ab0c9a2810ae654ff7017d4c1daa39a2ad5d2
web-sonic <asvodyanov@gmail.com>
2021-03-19 18:11:49 +0300

    writing parser

---
+++ lexer.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctragula <ctragula@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/17 18:08:04 by ctragula          #+#    #+#             */
+/*   Updated: 2021/03/19 18:11:39 by ctragula         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXER_H
+# define LEXER_H
+
+/* 
+** константы для парсера
+*/
+# define SPACES " \t"
+# define QUOTE 39
+# define DQUOTE 34
+# define PIPE 124
+# define SEMICOLON 59
+# define GREAT 62
+# define LOW 60
+# define STOP_SYMBOLS "\"'|<>; \t"
+#endif
