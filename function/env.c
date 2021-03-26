e849e8bd316ace169105094679814684c407535b
hille2 <hille.2@ya.ru>
2021-03-26 22:03:48 +0300

    unset+-

--- function/env.c
+++ function/env.c
@@ -6,20 +6,30 @@
 /*   By: yu <yu@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 11:08:45 by sgath             #+#    #+#             */
-/*   Updated: 2021/03/26 20:22:41 by yu               ###   ########.fr       */
+/*   Updated: 2021/03/26 21:43:40 by yu               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 void
-	ft_env(char **env)
-{
-	int		i;
-	t_list	*env_lst;
+	ft_env(void)
+// {
+// 	int		i;
+// 	t_list	*env_lst;
 
-	i = -1;
-	while (env[++i])
-		ft_lstadd_back(&env_lst, ft_lstnew(env[i]));
+// 	i = -1;
+// 	while (env[++i])
+// 		ft_lstadd_back(&env_lst, ft_lstnew(env[i]));
 	
+// }
+{
+	t_env *enviroment;
+
+	while(g_lstenv->next)
+	{
+		enviroment = g_lstenv->content;
+		g_lstenv = g_lstenv->next;
+	}
+	enviroment = g_lstenv->content;
 }
