af2c38ce56d671b55509af04b5291177217c3740
Change Tragula <ctragula@un-j6.kzn.21-school.ru>
2021-04-12 17:55:01 +0300

    hello world

--- srcs/execute/execute.c
+++ srcs/execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: ctragula <ctragula@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 13:49:34 by ctragula          #+#    #+#             */
-/*   Updated: 2021/04/12 17:28:45 by ctragula         ###   ########.fr       */
+/*   Updated: 2021/04/12 17:54:11 by ctragula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ static char
 	free(path);
 	i = 0;
 	cmd_name = 0;
+	if (!paths[i] && (g_error = 127))
+		file_error(cmd[0], "No such file or directory");
 	while (paths[i])
 	{
 		cmd_name = ft_strjoin(paths[i], "/");
@@ -72,14 +74,12 @@ static void
 		if (execve(cmd, args, env) == -1)
 			exit(0);
 	}
-	else if (ret < 0 && (g_error = 127))
-		file_error(args[0], "No such file or directory");
 	else if (ret > 0)
 		wait(&h);
 	ft_wordtab_clear(env);
 	if (ret > 0 && g_error != 130 && g_error != 131)
 		g_error = h / 256;
-	else if (cmd && ft_strncmp(args[0], cmd, ft_strlen(cmd) + 1))
+	if (cmd && ft_strncmp(args[0], cmd, ft_strlen(cmd) + 1))
 		free(cmd);
 }
 //обработать ошибку!
